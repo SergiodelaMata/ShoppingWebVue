e57d7b50188176292502568e9d754e6d569df283
SergiodelaMata <sergio.matam@edu.uah.es>
2022-04-13 12:35:45 +0200

    :sparkles: Added references to get the data from the json file for the pages related to adding a new product and adding a new category.

--- shopping-web-vue/src/components/NewProductPage.vue
+++ shopping-web-vue/src/components/NewProductPage.vue
@@ -1,9 +1,31 @@
 <script setup>
 import Navbar from '../components/Navbar.vue'
 import Title from '../components/Title.vue'
+import data from '../js/data.js'
+</script>
+<script>
+var props = data.data().props;
+var user = props.user;
+var products = props.products;
+var categories = props.categories;
+var users = props.users;
+var productsInBag = props.productsInBag;
+export default{
+    data() {
+        return {
+            props: {
+                user: user,
+                products: products,
+                categories: categories,
+                users: users,
+                productsInBag: productsInBag
+            }
+        }
+    }
+}
 </script>
 
 <template>
-  <Navbar/>
+  <Navbar v-bind:user="user" v-bind:users="users"/>
   <Title title="Introduzca un nuevo producto"/>
 </template>
