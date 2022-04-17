77a1214245288ab70a1d7a415b515b57b884e4be
SergiodelaMata <sergio.matam@edu.uah.es>
2022-04-17 13:52:01 +0200

    :sparkles: Created structure for the total cost of the products to be bought.

--- shopping-web-vue/src/components/TotalCost.vue
+++ shopping-web-vue/src/components/TotalCost.vue
@@ -16,4 +16,17 @@ export default{
 </script>
 
 <template>
+  <div className='card'>
+    <div className='row'>
+      <div className='col-sm-12 col-md-6 col-lg-6'>
+        <h5 style="margin-left: 0.5em, margin-top: 0.5em, margin-bottom: 0.5em">Coste Total:</h5>
+      </div>
+      <div className='col-sm-12 col-md-6 col-lg-6' style="textAlign: right">
+        <p style="margin-right: 0.5em, margin-top: 0.5em, margin-bottom: 0.5em"><!--{totalPrice}--> €</p>
+      </div>
+      <div className='col-sm-12 col-md-12 col-lg-12' style="width: 100%">
+        <button className='btn btn-primary' type='button' title='Realizar pedido' @click="this.setPopUp" style="width: 100%">Realizar pedido</button>
+      </div>
+    </div>
+  </div>
 </template>
